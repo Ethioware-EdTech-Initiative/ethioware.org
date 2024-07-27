aedd1739834afa1a7408bb329131ba8a69dbddc2
Anteneh Fisseha Yimmam <Antenehfish@gmail.com>
2024-07-27 22:29:19 +0300

    style: rebrand application

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,10 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
   const wrapper = document.getElementById("partner-wrapper");
   const content = wrapper.innerHTML;
   wrapper.innerHTML = content.repeat(3);
-  
+
   const contentWidth = wrapper.scrollWidth / 2;
-  const animationDuration = contentWidth / 100;
-  
-  wrapper.style.setProperty('--animation-duration', `${animationDuration}s`);
+  const animationDuration = contentWidth / 80;
+
+  wrapper.style.setProperty("--animation-duration", `${animationDuration}s`);
   wrapper.style.animation = `scroll var(--animation-duration) linear infinite`;
 });
