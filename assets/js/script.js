63693eb4f6fff437a98cea276bf59212d2a39463
Anteneh Fisseha Yimmam <Antenehfish@gmail.com>
2024-08-18 18:33:28 +0300

    style: assets

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,10 @@
 document.addEventListener("DOMContentLoaded", () => {
   const wrapper = document.getElementById("partner-wrapper");
   const content = wrapper.innerHTML;
-  wrapper.innerHTML = content.repeat(3);
+  wrapper.innerHTML = content.repeat(10);
 
-  const contentWidth = wrapper.scrollWidth / 2;
-  const animationDuration = contentWidth / 80;
+  const contentWidth = wrapper.scrollWidth / 1;
+  const animationDuration = contentWidth / 100;
 
   wrapper.style.setProperty("--animation-duration", `${animationDuration}s`);
   wrapper.style.animation = `scroll var(--animation-duration) linear infinite`;
