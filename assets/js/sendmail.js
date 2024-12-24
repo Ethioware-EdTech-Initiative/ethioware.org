e5168b85fac26b4394b8acf284f601b052472563
Anteneh Fisseha Yimmam <Antenehfish@gmail.com>
2024-12-24 17:04:39 +0300

    fix: debug the email js

--- assets/js/sendmail.js
+++ assets/js/sendmail.js
@@ -1,34 +1,36 @@
-function sendmessage() {
-  const email = document.getElementById("email").value;
-  const subject = document.getElementById("subject").value;
-  const message = document.getElementById("message").value;
-  // Validate form fields
-  if (!email || !subject || !message) {
-    alert("Please fill in all required fields.");
-    return;
-  }
+function sendMail(event) {
+    event.preventDefault();
 
-  // Validate email address format (optional)
-  const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
-  if (!emailRegex.test(email)) {
-    alert("Invalid email address.");
-    return;
-  }
+    const email = document.getElementById("email").value;
+    const subject = document.getElementById("subject").value;
+    const message = document.getElementById("message").value;
 
-  const parms = {
-    email,
-    subject,
-    message,
-  };
+    // Validate form fields
+    if (!email || !subject || !message) {
+        alert("Please fill in all required fields.");
+        return;
+    }
 
-  emailjs
-    .send("service_hurrcev", "template_bayaxj4", parms)
-    .then(() => {
-      alert("Email sent successfully!");
-    })
-    .catch((error) => {
-      alert(
-        "An error occurred while sending the email. Please try again later"
-      );
-    });
+    // Validate email address format (optional)
+    const emailRegex = /^[^\s@]+@[^\s@]+\.[^\s@]+$/;
+    if (!emailRegex.test(email)) {
+        alert("Invalid email address.");
+        return;
+    }
+
+    const parms = {
+        email,
+        subject,
+        message,
+    };
+
+    emailjs.send("service_vgahbke", "template_bayaxj4", parms)
+        .then(() => {
+            alert("Email sent successfully!");
+            document.getElementById('contactForm').reset();
+        })
+        .catch((error) => {
+            console.error("Error sending email:", error);
+            alert(`An error occurred while sending the email: ${error.text}`);
+        });
 }
