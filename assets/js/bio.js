27cebffa32e292d175033ae227db2cf10937a1f4
Samuel Worash Getaneh <samuelworash@gmail.com>
2025-05-24 21:49:21 +0300

    feat: experience section added, fix: increased responsiveness

--- assets/js/bio.js
+++ assets/js/bio.js
@@ -2,7 +2,8 @@ document.addEventListener("DOMContentLoaded", () => {
     const menuToggle = document.querySelector("#nav-toggle");
     const menu = document.querySelector("#nav-menu");
     const menuClose = document.querySelector("#nav-close");
-
+    const desc = document.querySelectorAll(".job-desc");
+    const descToggle = document.querySelectorAll(".tag-close");
 
 
 
@@ -14,5 +15,11 @@ document.addEventListener("DOMContentLoaded", () => {
     });
     menuClose.addEventListener("click", e => {
         menu.classList.remove("show-menu");
+    });
+    descToggle.forEach((toggle, ind) => {
+        toggle.addEventListener("click", e => {
+            desc[ind].classList.toggle("hidden");
+            toggle.classList.toggle("turn");
+        });
     });
 });
